373a42b46c3f28f5d6e57032fb2cf04a38ec3ead
vulfke482 <denis.kartashov@482.solutions>
2019-09-03 15:27:22 +0300

    Added currency again

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
-const SafeMath = artifacts.require("SafeMath");
-const ERC20Token = artifacts.require("ERC20Token");
+const Currency = artifacts.require("Currency");
 const Project = artifacts.require("Project");
 const Intermediary = artifacts.require("Intermediary");
 
 module.exports = function(deployer) {
 
-
+  deployer.deploy(Currency);
+  deployer.link(Currency, Intermediary);
+  
   deployer.deploy(Project, "project", 0, "project", 0, 0);
   deployer.link(Project, Intermediary);
 
